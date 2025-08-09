b78718befdc300ff27ec1c7bf36e1ede37432708
Hiroki Matsueda <matsueda.hiroki@gmail.com>
2025-08-09 23:15:05 +0900

    refactor: Adopt properties in DeviceManager

--- games/device_manager.py
+++ games/device_manager.py
@@ -13,18 +13,38 @@ class DeviceManager:
 
     def __init__(self):
         # LEDマトリクス初期化
-        self.i2c_0 = busio.I2C(board.GP17, board.GP16)
-        self.matrix = Matrix8x8x2(self.i2c_0)
+        self._i2c_0 = busio.I2C(board.GP17, board.GP16)
+        self._matrix = Matrix8x8x2(self._i2c_0)
 
         # 7セグメントディスプレイ初期化（必要ならコメント解除）
-        # self.i2c_1 = busio.I2C(board.GP15, board.GP14)
-        # self.seg = Seg7x4(self.i2c_1)
+        # self._i2c_1 = busio.I2C(board.GP15, board.GP14)
+        # self._seg = Seg7x4(self._i2c_1)
 
         # ボタン初期化
-        self.button_a = digitalio.DigitalInOut(board.GP0)
-        self.button_a.direction = digitalio.Direction.INPUT
-        self.button_a.pull = digitalio.Pull.UP
+        self._button_a = digitalio.DigitalInOut(board.GP0)
+        self._button_a.direction = digitalio.Direction.INPUT
+        self._button_a.pull = digitalio.Pull.UP
 
-        self.button_b = digitalio.DigitalInOut(board.GP1)
-        self.button_b.direction = digitalio.Direction.INPUT
-        self.button_b.pull = digitalio.Pull.UP
+        self._button_b = digitalio.DigitalInOut(board.GP1)
+        self._button_b.direction = digitalio.Direction.INPUT
+        self._button_b.pull = digitalio.Pull.UP
+
+    @property
+    def matrix(self) -> Matrix8x8x2:
+        """LEDマトリクスへのアクセス"""
+        return self._matrix
+
+    # 7セグメントディスプレイ（必要ならコメント解除）
+    # @property
+    # def seg(self) -> Seg7x4:
+    #     return self._seg
+
+    @property
+    def button_a(self) -> digitalio.DigitalInOut:
+        """Aボタンへのアクセス"""
+        return self._button_a
+
+    @property
+    def button_b(self) -> digitalio.DigitalInOut:
+        """Bボタンへのアクセス"""
+        return self._button_b
